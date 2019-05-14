d883772fcbb049eebed420551c67445c89c231aa
Pooja Kumari <kumarip@codeaurora.org>
2019-05-14 13:23:16 +0530

    X2: Build IPACM (IP Catalogue and IP Management)
    Change-Id: I6b73948ed0658f512688fe6bf66a1b029f40390a

--- device.mk
+++ device.mk
@@ -126,7 +126,12 @@ PRODUCT_PACKAGES += \
     libhidltransport.vendor \
     libhwbinder \
     libhwbinder.vendor
-    
+
+# IPACM
+PRODUCT_PACKAGES += \
+    ipacm \
+    IPACM_cfg.xml
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
