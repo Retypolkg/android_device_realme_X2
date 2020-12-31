7bf97ec84756a8359b56b2a5ff0db15ddfd29ac1
Bruno Martins <bgcngm@gmail.com>
2020-12-31 00:46:08 +0000

    X2: Use CodeAurora ImsService implementation for RCS
    There is no evidence that Qualcomm devices running a rather recent IMS
    stack need to include AOSP RCS packages. RCS already worked before this
    change and continues to do so, but the main goal was to get rid of some
    of the RCS related log spam, which was accomplished here.
    
    Change-Id: I219546e29d288373877e9bcd194a60ef5e09f224

--- device.mk
+++ device.mk
@@ -305,12 +305,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml
 
-# RCS
-PRODUCT_PACKAGES += \
-    com.android.ims.rcsmanager \
-    PresencePolling \
-    RcsService
-
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
