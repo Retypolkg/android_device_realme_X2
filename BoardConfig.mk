da152e6965ae8fe32ddbe484181f6bee6f9a61f2
Edwin Moquete <edwinmmoquete@gmail.com>
2022-03-04 18:09:08 -0400

    X2: Set TARGET_TAP_TO_WAKE_NODE
    Change-Id: I971349c0aaf6b11231ce79cd3260dd758bd88a2b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -120,6 +120,9 @@ BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := sm6150
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno618
 
+# Power
+TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
+
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
