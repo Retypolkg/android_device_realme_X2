d1633b558a5a002a66f3f096ed7b9a635fa4c116
Retypolkg <retypolkg2021@gmail.com>
2022-05-14 20:48:22 +0200

    X2: sensors: Set correct type for proximity sensor
    * Replace proprietary sensor type with standard one
    
    Signed-off-by: Retypolkg <retypolkg2021@gmail.com>

--- sensors/HalProxy.cpp
+++ sensors/HalProxy.cpp
@@ -484,6 +484,10 @@ void HalProxy::initializeSensorList() {
                         sensor.type = SensorType::LIGHT;
                         sensor.typeAsString = "";
                     }
+                    if (sensor.typeAsString == "android.sensor.tp_proximity") {
+                        sensor.type = SensorType::PROXIMITY;
+                        sensor.typeAsString = "";
+                    }
                     mSensors[sensor.sensorHandle] = sensor;
                 }
             }
