faed677b0b22fbc6d3abb50d94cc4fe33191bd83
Retypolkg <retypolkg2021@gmail.com>
2022-10-08 19:38:54 +0200

    X2: Initial skeleton tree

---
+++ BoardConfig.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/realme/X2
+
+# Inherit from the proprietary version
+include vendor/realme/X2/BoardConfigVendor.mk
---
+++ device.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from vendor if exists
+$(call inherit-product-if-exists, vendor/realme/X2/X2-vendor.mk)
