c547967b017d8cf138cefcc34e9a11ca43682a05
Retypolkg <retypolkg2021@gmail.com>
2022-10-08 20:45:05 +0200

    X2: Import config.fs
    * From RMX1993_F.26

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,6 +24,9 @@ TARGET_2ND_CPU_VARIANT := cortex-a9
 TARGET_BOOTLOADER_BOARD_NAME := sm6150
 TARGET_NO_BOOTLOADER := true
 
+# Filesystem
+TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
+
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 androidboot.usbcontroller=a600000.dwc3 earlycon=msm_geni_serial,0x880000 loop.max_part=7
--- device.mk
+++ device.mk
@@ -4,5 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# AID/fs configs
+PRODUCT_PACKAGES += \
+    fs_config_files
+
 # Inherit from vendor if exists
 $(call inherit-product-if-exists, vendor/realme/X2/X2-vendor.mk)
