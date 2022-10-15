02bd21de3e39187ac0533f3a73387beecbde7be9
Retypolkg <retypolkg2021@gmail.com>
2022-10-15 15:09:08 +0200

    X2: Specify pixel densiti

--- device.mk
+++ device.mk
@@ -4,6 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# AAPT
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
 # AID/fs configs
 PRODUCT_PACKAGES += \
     fs_config_files
