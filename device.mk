3495823583824a5ea494b99372c4db8b2520cdfc
Retypolkg <retypolkg2021@gmail.com>
2022-10-15 13:42:37 +0200

    X2: Define boot animation resolution

--- device.mk
+++ device.mk
@@ -12,5 +12,9 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_PACKAGES += \
     fs_config_files
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2240
+TARGET_SCREEN_WIDTH := 1080
+
 # Inherit from vendor if exists
 $(call inherit-product-if-exists, vendor/realme/X2/X2-vendor.mk)
