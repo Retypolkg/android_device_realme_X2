bd4b7ba1c8edf9aa5e7ab54c8dca7c86c1c18b2b
Retypolkg <retypolkg2021@gmail.com>
2022-11-05 21:50:37 +0100

    X2: Import sec config
    * From RMX1993_F.26

--- device.mk
+++ device.mk
@@ -132,6 +132,10 @@ PRODUCT_PACKAGES += \
     ipacm \
     IPACM_cfg.xml
 
+# IPC router config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
