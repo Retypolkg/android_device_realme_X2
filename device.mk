c36aee926aa1ea9e6ab4182bbdf3d783f768d876
Retypolkg <retypolkg2021@gmail.com>
2022-11-05 22:34:17 +0100

    X2: Import media configs
    * From RMX1993_F.26

--- device.mk
+++ device.mk
@@ -161,6 +161,10 @@ PRODUCT_PACKAGES += \
     libstagefright_softomx.vendor
 
 PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/media/odm/,$(TARGET_COPY_OUT_ODM)/etc) \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/media/vendor,$(TARGET_COPY_OUT_VENDOR)/etc)
+
+PRODUCT_COPY_FILES += \
     hardware/qcom-caf/sm8150/media/conf_files/sm6150/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     hardware/qcom-caf/sm8150/media/conf_files/sm6150/system_properties.xml:$(TARGET_COPY_OUT_VENDOR)/etc/system_properties.xml
 
