f2ed5e5c023d6f1ed7be6aab3c6e1c73fa3c487a
Retypolkg <retypolkg2021@gmail.com>
2022-11-06 15:19:59 +0100

    X2: Build servicetracker libs

--- device.mk
+++ device.mk
@@ -321,6 +321,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
+# Service Tracker
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.servicetracker@1.2.vendor
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
