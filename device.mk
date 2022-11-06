a1c4b3c5cc444582ce0aeee44d42320b870d27ff
Retypolkg <retypolkg2021@gmail.com>
2022-11-06 15:17:43 +0100

    X2: Set PRODUCT_CHARACTERISTICS := nosdcard

--- device.mk
+++ device.mk
@@ -11,6 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+PRODUCT_CHARACTERISTICS := nosdcard
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
