8db984f86c8c46c55ccaee5caddc4a90c56bd196
Retypolkg <retypolkg2021@gmail.com>
2022-11-06 20:21:22 +0100

    X2: Setup overlay

--- device.mk
+++ device.mk
@@ -13,6 +13,7 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_CHARACTERISTICS := nosdcard
