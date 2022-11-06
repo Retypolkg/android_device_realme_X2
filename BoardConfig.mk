1f0ec7fa723b4f008b8bf4e2edce95fb4a41f0e3
Retypolkg <retypolkg2021@gmail.com>
2022-11-06 16:14:16 +0100

    [TMP] X2: Ignore selinux neverallows

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -141,6 +141,7 @@ VENDOR_SECURITY_PATCH := 2021-10-05
 TARGET_SEPOLICY_DIR := msmsteppe
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
