593a6b5f3c72b249b012285ee40c5b8e77383f8c
Retypolkg <retypolkg2021@gmail.com>
2022-11-06 15:22:08 +0100

    X2: Import qti_whitelist
    * From RMX1993_F.26

--- device.mk
+++ device.mk
@@ -286,6 +286,10 @@ PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti \
     libvndfwk_detect_jni.qti.vendor
 
+# QTI
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
+
 # RCS
 PRODUCT_PACKAGES += \
     com.android.ims.rcsmanager \
