8be83cf5e8d4edf0869572e2fb23f532be685305
Retypolkg <retypolkg2021@gmail.com>
2022-11-06 16:33:16 +0100

    X2: Define TARGET_SURFACEFLINGER_UDFPS_LIB

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -147,6 +147,9 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SELINUX_IGNORE_NEVERALLOWS := true
 
+# UDFPS
+TARGET_SURFACEFLINGER_UDFPS_LIB := //$(DEVICE_PATH):libudfps_extension.x2
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
