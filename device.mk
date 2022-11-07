ee6de9f5f656033cd04967fedd37604cd4a2ab11
Retypolkg <retypolkg2021@gmail.com>
2022-11-07 19:44:22 +0100

    X2: Build camera stack

--- device.mk
+++ device.mk
@@ -47,6 +47,15 @@ PRODUCT_PACKAGES += \
 TARGET_SCREEN_HEIGHT := 2240
 TARGET_SCREEN_WIDTH := 1080
 
+# Camera
+PRODUCT_PACKAGES += \
+    android.frameworks.displayservice@1.0 \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_64 \
+    libgui_vendor \
+    libstdc++.vendor \
+    vendor.qti.hardware.camera.device@1.0.vendor
+
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
