64127de7c61ef0451f6aba39cc9f37a30bb2ecf0
npjohnson <johnsonnolen@gmail.com>
2022-11-11 21:22:58 +0100

    X2: Move to libhidl shim in hardware/lineage/compat
    Change-Id: Ia2cd305baf3ce0fa7ac52d440a4ff51f94654438

--- device.mk
+++ device.mk
@@ -179,9 +179,7 @@ PRODUCT_COPY_FILES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
     android.hidl.base@1.0.vendor \
-    android.hidl.manager@1.0 \
     android.hidl.manager@1.0.vendor
 
 PRODUCT_PACKAGES += \
