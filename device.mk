59f4a16dc3e76faa18c0aa5b47e43cffef9a855a
Sukrut4778 <sukrut4778@gmail.com>
2022-12-06 10:41:34 +0100

    X2: Build audio.primary.default

--- device.mk
+++ device.mk
@@ -71,6 +71,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.soundtrigger@2.1-impl \
     audio.bluetooth.default \
+    audio.primary.default \
     audio.r_submix.default \
     audio.usb.default \
     libqcompostprocbundle \
