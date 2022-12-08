ec624e71f7ef57e678a82598668568bb2cbe65a0
Retypolkg <retypolkg2021@gmail.com>
2022-12-08 21:04:09 +0100

    X2: Import HotwordEnrollment blobs

--- device.mk
+++ device.mk
@@ -176,10 +176,6 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
 
-# HotwordEnrollement app permissions
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
-
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0.vendor \
