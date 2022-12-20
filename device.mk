047b99c808579621e1d039149ddfda891d93dc7c
Retypolkg <retypolkg2021@gmail.com>
2022-12-20 10:30:37 +0100

    X2: Build libcamera2ndk_vendor from source

--- device.mk
+++ device.mk
@@ -110,6 +110,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
+    libcamera2ndk_vendor \
     libgui_vendor \
     libstdc++.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor
