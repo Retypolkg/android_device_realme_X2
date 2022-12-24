2798c505eebd91c5dec4c97ac5fdd21101832c59
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2022-12-24 13:22:53 +0100

    X2: location: Exclude loc_{api,socket} on OSS builds
    Change-Id: Id918ca897805d11b07558d600066990e93d95666

--- location/loc_api/Android.mk
+++ location/loc_api/Android.mk
@@ -1,2 +1,8 @@
+ifneq ($(QCPATH),)
+ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
+
 LOCAL_PATH := $(call my-dir)
 include $(call all-subdir-makefiles)
+
+endif #BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE
+endif #QCPATH
--- location/utils/Android.mk
+++ location/utils/Android.mk
@@ -1,2 +1,4 @@
+ifneq ($(QCPATH),)
 LOCAL_PATH := $(call my-dir)
 include $(call all-makefiles-under,$(LOCAL_PATH))
+endif #QCPATH
