2c9e39363203b0db2f9ec0e43962cfbee7260acc
Retypolkg <retypolkg2021@gmail.com>
2023-03-08 14:51:16 +0100

    X2: Deduplicate audio.primary.default
    * It's building by handheld_vendor.mk

--- device.mk
+++ device.mk
@@ -71,7 +71,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.soundtrigger@2.1-impl \
     audio.bluetooth.default \
-    audio.primary.default \
     audio.r_submix.default \
     audio.usb.default \
     liba2dpoffload \
